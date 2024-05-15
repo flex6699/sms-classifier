927e61b4e656de4100f5858b986942b5fd3ebb38
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-05-15 10:41:00 +0530

    final v

---
+++ main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import joblib
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+
+
+# Load TF-IDF vectorizer
+tfidf = joblib.load('vectorizer.joblib')
+
+# Load model
+model = joblib.load('model.joblib')
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    print(transformed_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
